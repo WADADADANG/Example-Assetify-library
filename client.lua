c5a155f9b1ca8dddd1ff125277c6c46efb5543cc
WADADADANG <kitsada19972540@gmail.com>
2025-05-27 04:53:18 +0700

    0

--- client.lua
+++ client.lua
@@ -2,10 +2,11 @@
 
 
 --[[
+    client side
+
     sound.play()
     https://github.com/ov-sa/Assetify.library/wiki/API:-Sound#assetifysoundplay---client
 
-    client side
 
     example path folder
     -----------------------------------------------------
@@ -35,3 +36,36 @@ local play = {
 }
 
 local cSound = assetify.sound.play( play.assetName, play.soundCategory, play.soundIndex, play.soundVolume, play.isScoped, play.isLooped )
+
+
+--[[
+    shared side
+
+    getAssetDep()
+    https://github.com/ov-sa/Assetify.library/wiki/API:-Sound#assetifysoundplay---client
+
+    example path folder
+    -----------------------------------------------------
+    pathfile: assetify_library\files\assets\module\assetName_example\dep\WADADADANG.png
+    -----------------------------------------------------
+
+    example asset.vcl
+    -----------------------------------------------------
+    encryptKey: "XqtcfmbZ9oZU8Ton"
+    encryptMode: "aes128"
+
+    assetDeps:
+        logo:
+            -: "WADADADANG.png"
+    -----------------------------------------------------
+]]--
+
+
+local DepImage = {}
+DepImage.assetName = "module"
+DepImage.assetType = "image"
+DepImage.depType = "logo"
+DepImage.depIndex = 1
+DepImage.depSubIndex = false -- Optional 
+
+local cDep = assetify.getAssetDep( DepImage.assetName, DepImage.assetType, DepImage.depType, DepImage.depIndex, DepImage.depSubIndex )
---
+++ folder_example/module/image/asset.vcl
@@ -0,0 +1,6 @@
+encryptKey: "XqtcfmbZ9oZU8Ton"
+encryptMode: "aes128"
+
+assetDeps:
+    logo:
+        -: "WADADADANG.png"
